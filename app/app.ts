37ceb9561138e35e395ce486b20c594124a01972
victor <2006victorlegal@gmail.com>
2023-10-27 09:08:25 -0300

    Fazndo ultimos ajustes.

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,6 @@
-const operadores = ['+', '-', 'x', '/'];
-const operadoresPrioritarios = ['x', '/'];
+const operadores = ['+', '-', '*', '/'];
+const operadoresPrioritarios = [ '*', '/'];
+const operadoresNormais = ['+', '-'];
 
 class Calculadora {
     constructor(protected pilha: Array<string | number>, protected display?: HTMLInputElement) {
@@ -35,61 +36,48 @@ class Calculadora {
 
     public adicionarAPilha(valor: string | number): void {
         const ultimoItem = this.pilha[this.pilha.length - 1];
-
+    
         if (typeof valor === 'string' && operadores.includes(valor)) {
             if (this.pilha.length === 0) {
-                return; 
+                return;
             }
-
-            if (operadores.includes(ultimoItem as string)) {
-                this.setarOperador(valor.toString());
-            } else {
-                this.pilha.push(valor.toString());
-            }
-        } else if (typeof valor === 'number') {
-            if (typeof ultimoItem === 'number') {
-                this.pilha[this.pilha.length - 1] = parseFloat(ultimoItem.toString() + valor.toString());
-            } else {
-                this.setarValor(valor);
-            }
-        } else if (typeof valor === 'string' && !operadores.includes(valor)) {
-            const numeroComVirgula = parseFloat(valor.replace(',', '.'));
-            if (!isNaN(numeroComVirgula)) {
-                if (typeof ultimoItem === 'number') {
-                    this.pilha[this.pilha.length - 1] = parseFloat(ultimoItem.toString() + numeroComVirgula.toString());
+            this.setarOperador(valor as string);
+        } else {
+            if (typeof valor === 'string') {
+                if (typeof ultimoItem === 'string' && !operadores.includes(ultimoItem)) {
+                    this.pilha[this.pilha.length - 1] = `${ultimoItem}${valor}`;
                 } else {
-                    this.setarValor(numeroComVirgula);
+                    this.pilha.push(`${valor}`);
                 }
             }
         }
-
-        this.setarDisplay(this.pilha.join(' ').replace('.', ','));
+    
         console.log(this.pilha);
+        this.setarDisplay(this.pilha.join(' '));
     }
     
     public calcular() {
         const memoriaTemporaria = [];
-        const operadoresNormais = ['+', '-'];
-
+    
         let tempOperador = null;
         let tempOperando = null;
-
+    
         this.pilha.forEach((elemento) => {
-            if (typeof elemento === 'number') {
+            if (typeof elemento === 'number' || (typeof elemento === 'string' && !operadores.includes(elemento))) {
                 if (tempOperando === null) {
                     tempOperando = elemento;
                 } else {
                     if (tempOperador) {
-                        if (tempOperador === 'x') {
-                            tempOperando *= elemento;
+                        if (tempOperador === '*') {
+                            tempOperando = parseFloat(tempOperando.toString().replace(',', '.')) * parseFloat(elemento.toString().replace(',', '.'));
                         } else if (tempOperador === '/') {
-                            if (elemento === 0) {
+                            if (parseFloat(elemento.toString().replace(',', '.')) === 0) {
                                 this.pilha = [];
                                 this.setarDisplay('Não é possível dividir por 0');
                                 setTimeout(() => { this.setarDisplay('') }, 2000);
                                 return;
                             } else {
-                                tempOperando /= elemento;
+                                tempOperando = parseFloat(tempOperando.toString().replace(',', '.')) / parseFloat(elemento.toString().replace(',', '.'));
                             }
                         } else {
                             tempOperador = null;
@@ -104,36 +92,37 @@ class Calculadora {
                 tempOperando = null;
             }
         });
-
+    
         if (tempOperando !== null) {
             memoriaTemporaria.push(tempOperando);
         }
-
+    
         if (this.pilha.length > 0) {
-            let resultado = memoriaTemporaria[0];
+            let resultado = parseFloat(memoriaTemporaria[0].toString().replace(',', '.'));
             for (let i = 1; i < memoriaTemporaria.length; i += 2) {
                 const operador = memoriaTemporaria[i];
                 const operando = memoriaTemporaria[i + 1];
                 if (operador === '+') {
-                    resultado += operando;
+                    resultado += parseFloat(operando.toString().replace(',', '.'));
                 } else if (operador === '-') {
-                    resultado -= operando;
+                    resultado -= parseFloat(operando.toString().replace(',', '.'));
                 }
             }
-
+    
             if (isNaN(resultado)) {
                 this.pilha = [];
                 this.setarDisplay('Resultado inválido :(');
                 setTimeout(() => { this.setarDisplay('') }, 2000);
                 return;
             }
-
+    
             this.pilha = [];
             this.setarValor(resultado);
             this.setarDisplay(resultado.toString().replace('.', ','));
             console.log(`RESULTADO: ${resultado}`.replace('.', ','));
         }
     }
+    
 
     public limpar(): void {
         this.pilha = [];
@@ -144,17 +133,18 @@ class Calculadora {
     public mudarSinal(): void {
         if (this.pilha.length > 0) {
             const primeiroItem = this.pilha[0];
-
-            if (typeof primeiroItem === 'number') {
-                if (primeiroItem > 0) {
+    
+            if (typeof primeiroItem === 'number' || (typeof primeiroItem === 'string' && !operadores.includes(primeiroItem))) {
+                if (typeof primeiroItem === 'number') {
                     this.pilha[0] = -primeiroItem;
-                } else if (primeiroItem < 0) {
-                    this.pilha[0] = Math.abs(primeiroItem);
+                } else {
+                    this.pilha[0] = -parseFloat(primeiroItem.toString().replace(',', '.'));
                 }
                 this.setarDisplay(this.pilha.join(' '));
             }
         }
     }
+    
 
     public apagarDigito(): void {
         if (this.pilha.length > 0) {
@@ -181,7 +171,7 @@ class Calculadora {
 
 const calculadora: Calculadora = new Calculadora([]);
 
-const numeros: Array<string> = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ','];
+const numeros: Array<string> = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',', '.'];
 document.addEventListener("keydown", (evento: KeyboardEvent) => {
     evento.preventDefault();
     if (evento.key === "Enter") {
--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,6 @@
-const operadores = ['+', '-', 'x', '/'];
-const operadoresPrioritarios = ['x', '/'];
+const operadores = ['+', '-', '*', '/'];
+const operadoresPrioritarios = ['*', '/'];
+const operadoresNormais = ['+', '-'];
 class Calculadora {
     constructor(pilha, display) {
         this.pilha = pilha;
@@ -38,59 +39,44 @@ class Calculadora {
             if (this.pilha.length === 0) {
                 return;
             }
-            if (operadores.includes(ultimoItem)) {
-                this.setarOperador(valor.toString());
-            }
-            else {
-                this.pilha.push(valor.toString());
-            }
+            this.setarOperador(valor);
         }
-        else if (typeof valor === 'number') {
-            if (typeof ultimoItem === 'number') {
-                this.pilha[this.pilha.length - 1] = parseFloat(ultimoItem.toString() + valor.toString());
-            }
-            else {
-                this.setarValor(valor);
-            }
-        }
-        else if (typeof valor === 'string' && !operadores.includes(valor)) {
-            const numeroComVirgula = parseFloat(valor.replace(',', '.'));
-            if (!isNaN(numeroComVirgula)) {
-                if (typeof ultimoItem === 'number') {
-                    this.pilha[this.pilha.length - 1] = parseFloat(ultimoItem.toString() + numeroComVirgula.toString());
+        else {
+            if (typeof valor === 'string') {
+                if (typeof ultimoItem === 'string' && !operadores.includes(ultimoItem)) {
+                    this.pilha[this.pilha.length - 1] = `${ultimoItem}${valor}`;
                 }
                 else {
-                    this.setarValor(numeroComVirgula);
+                    this.pilha.push(`${valor}`);
                 }
             }
         }
-        this.setarDisplay(this.pilha.join(' ').replace('.', ','));
         console.log(this.pilha);
+        this.setarDisplay(this.pilha.join(' '));
     }
     calcular() {
         const memoriaTemporaria = [];
-        const operadoresNormais = ['+', '-'];
         let tempOperador = null;
         let tempOperando = null;
         this.pilha.forEach((elemento) => {
-            if (typeof elemento === 'number') {
+            if (typeof elemento === 'number' || (typeof elemento === 'string' && !operadores.includes(elemento))) {
                 if (tempOperando === null) {
                     tempOperando = elemento;
                 }
                 else {
                     if (tempOperador) {
-                        if (tempOperador === 'x') {
-                            tempOperando *= elemento;
+                        if (tempOperador === '*') {
+                            tempOperando = parseFloat(tempOperando.toString().replace(',', '.')) * parseFloat(elemento.toString().replace(',', '.'));
                         }
                         else if (tempOperador === '/') {
-                            if (elemento === 0) {
+                            if (parseFloat(elemento.toString().replace(',', '.')) === 0) {
                                 this.pilha = [];
                                 this.setarDisplay('Não é possível dividir por 0');
                                 setTimeout(() => { this.setarDisplay(''); }, 2000);
                                 return;
                             }
                             else {
-                                tempOperando /= elemento;
+                                tempOperando = parseFloat(tempOperando.toString().replace(',', '.')) / parseFloat(elemento.toString().replace(',', '.'));
                             }
                         }
                         else {
@@ -112,15 +98,15 @@ class Calculadora {
             memoriaTemporaria.push(tempOperando);
         }
         if (this.pilha.length > 0) {
-            let resultado = memoriaTemporaria[0];
+            let resultado = parseFloat(memoriaTemporaria[0].toString().replace(',', '.'));
             for (let i = 1; i < memoriaTemporaria.length; i += 2) {
                 const operador = memoriaTemporaria[i];
                 const operando = memoriaTemporaria[i + 1];
                 if (operador === '+') {
-                    resultado += operando;
+                    resultado += parseFloat(operando.toString().replace(',', '.'));
                 }
                 else if (operador === '-') {
-                    resultado -= operando;
+                    resultado -= parseFloat(operando.toString().replace(',', '.'));
                 }
             }
             if (isNaN(resultado)) {
@@ -143,12 +129,12 @@ class Calculadora {
     mudarSinal() {
         if (this.pilha.length > 0) {
             const primeiroItem = this.pilha[0];
-            if (typeof primeiroItem === 'number') {
-                if (primeiroItem > 0) {
+            if (typeof primeiroItem === 'number' || (typeof primeiroItem === 'string' && !operadores.includes(primeiroItem))) {
+                if (typeof primeiroItem === 'number') {
                     this.pilha[0] = -primeiroItem;
                 }
-                else if (primeiroItem < 0) {
-                    this.pilha[0] = Math.abs(primeiroItem);
+                else {
+                    this.pilha[0] = -parseFloat(primeiroItem.toString().replace(',', '.'));
                 }
                 this.setarDisplay(this.pilha.join(' '));
             }
@@ -178,7 +164,7 @@ class Calculadora {
     }
 }
 const calculadora = new Calculadora([]);
-const numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ','];
+const numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',', '.'];
 document.addEventListener("keydown", (evento) => {
     evento.preventDefault();
     if (evento.key === "Enter") {
